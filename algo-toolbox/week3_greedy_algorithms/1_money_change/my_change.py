ec9f10eeb6c2af17e25b2a220a20f3fcac615048
Ralph Plumley <ralphplumley@gmail.com>
2018-03-16 18:39:16 -0400

    Pass 1/6 (week 3 - Algo Toolbox)

--- algo-toolbox/week3_greedy_algorithms/1_money_change/my_change.py
+++ algo-toolbox/week3_greedy_algorithms/1_money_change/my_change.py
@@ -3,8 +3,13 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    output = (m // 10) + ( (m % 10) // 5 + (m % 5) )
+    # print('Dimes: ', m//10)
+    # print('Nickels', (m%10)//5)
+    # print('Pennies', m%5)
+    return output
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
-    print(get_change(m))
+    # m = int(sys.stdin.read())
+    input = int(input())
+    print(get_change(input))
