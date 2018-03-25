d70629515f32e0135533a8bf28a6d65a8f56feaf
Ralph Plumley <ralphplumley@gmail.com>
2018-03-25 17:49:38 -0400

    pass 1/6 binarySearch - (week 4)

--- algo-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ algo-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,17 +1,25 @@
 # Uses python3
 import sys
+import math
 
-# 3 1 5 8
-# 8 1 23
+# 8 1 3 5 5 7 9 11 13
+# 1 9
 
-def binary_search(a, target):
-    print('')
-    left, right = 0, len(a)
-    # write your code here
-    print('a: ', a)
-    print('target: ', target)
-    print('left index: ', left)
-    print('right index: ', right)
+def binary_search(arr, target):
+    minIndex, maxIndex = 0, len(arr) - 1
+
+    while maxIndex >= minIndex:
+        midIndex = math.floor((minIndex + maxIndex) / 2)
+
+        if arr[midIndex] == target:
+            return midIndex
+        elif arr[midIndex] < target:
+            minIndex = midIndex + 1
+        else:
+            maxIndex = midIndex - 1
+
+    return -1
+    
 
 def linear_search(a, target):
     for i in range(len(a)):
@@ -25,7 +33,7 @@ if __name__ == '__main__':
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
-    a = data[1 : n + 1]
-    for x in data[n + 2:]:
+    arr = data[1 : n + 1]
+    for target in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(binary_search(a, target), end = ' ')
+        print(binary_search(arr, target), end = ' ')
