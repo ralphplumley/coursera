ab9dc2d5b194670df1b72c5e64f4e49715c539ab
Ralph Plumley <ralphplumley@gmail.com>
2018-03-25 17:12:43 -0400

    printing 1/6 binarySearch - (week 4)

--- algo-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
+++ algo-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.py
@@ -1,22 +1,31 @@
 # Uses python3
 import sys
 
-def binary_search(a, x):
+# 3 1 5 8
+# 8 1 23
+
+def binary_search(a, target):
+    print('')
     left, right = 0, len(a)
     # write your code here
+    print('a: ', a)
+    print('target: ', target)
+    print('left index: ', left)
+    print('right index: ', right)
 
-def linear_search(a, x):
+def linear_search(a, target):
     for i in range(len(a)):
-        if a[i] == x:
+        if a[i] == target:
             return i
     return -1
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    input = sys.stdin.read() # for Coursera submission
+
     data = list(map(int, input.split()))
     n = data[0]
     m = data[n + 1]
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, target), end = ' ')
