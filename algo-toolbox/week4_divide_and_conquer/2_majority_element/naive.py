4e4f8a2843aac7c5c671c6ad8de022ca1012f32e
Ralph Plumley <ralphplumley@gmail.com>
2018-03-28 19:19:23 -0400

    implement naive.py

--- algo-toolbox/week4_divide_and_conquer/2_majority_element/naive.py
+++ algo-toolbox/week4_divide_and_conquer/2_majority_element/naive.py
@@ -7,12 +7,28 @@ def get_majority_element(a, left, right):
     if left + 1 == right:
         return a[left]
     #write your code here
-    return 1
+    
+    for i in range(len(a)):
+        currentElement = a[i]
+        count = 0
+
+        for j in range(len(a)):
+            if a[j] == currentElement:
+                count += 1
+
+    half = len(a) / 2
+    if count > half:
+        print('currentElement: ', currentElement)
+        return 1
+
+    return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, 0, n) == 1:
         print(1)
-    else:
+    elif get_majority_element(a, 0, n) == 0:
         print(0)
+    else:
+        print('error')
