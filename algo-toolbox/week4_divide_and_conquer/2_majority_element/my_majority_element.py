445bd6778c040d4d44abf9b91c088479a9327fb4
Ralph Plumley <ralphplumley@gmail.com>
2018-03-30 11:08:03 -0400

    attempt majority element with Dictionary

--- algo-toolbox/week4_divide_and_conquer/2_majority_element/my_majority_element.py
+++ algo-toolbox/week4_divide_and_conquer/2_majority_element/my_majority_element.py
@@ -1,18 +1,43 @@
 # Uses python3
 import sys
+import math
 
 def get_majority_element(a, left, right):
     if left == right:
         return -1
     if left + 1 == right:
         return a[left]
+
     #write your code here
-    return -1
+
+    if len(a) // 2 != 0:
+        numForMajority = (math.ceil(len(a) / 2 )) + 1
+    else:
+        numForMajority = math.ceil(len(a) / 2 )
+
+        
+    count = {}
+
+    for i in range(len(a)):
+        currentElement = a[i]
+        if currentElement in count:
+            count[currentElement] += 1
+
+            if count[currentElement] == numForMajority:
+                return 1
+        else:
+            count[currentElement] = 1
+
+    print('count: ', count)
+
+    return 0
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    if get_majority_element(a, 0, n) == 1:
         print(1)
-    else:
+    elif get_majority_element(a, 0, n) == 0:
         print(0)
+    else:
+        print('error')
